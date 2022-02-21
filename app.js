776621d3b3a288525f82c1f219fca8d72be94d80
trinita21 <trinita20221@iiitd.ac.in>
2022-02-21 19:01:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("handle-submit");
+const out = document.getElementById("output");
+const calc = (CP, SP, qty) => {
+  answer = (SP - CP) * qty;
+  answer_percent = ((SP - CP) / CP) * 100;
+  if (answer > 0)
+    output.innerText =
+      "Profit:" + answer + " " + "Profit Percent:" + answer_percent + "%";
+  else if (answer < 0)
+    output.innerText =
+      "Loss:" +
+      Math.abs(answer) +
+      " " +
+      "Loss Percent:" +
+      Math.abs(answer_percent) +
+      "%";
+  else output.innerText = "Ye bik gyi hai gormint";
+};
+
+const profit_loss = () => {
+  console.log("abc");
+  const CP = document.getElementById("cost_p").value;
+  const SP = document.getElementById("selling_p").value;
+  const qty = document.getElementById("quantity").value;
+  if (CP === "" || SP === "" || qty === "")
+    output.innerText = "No value has been provided";
+  else calc(CP, SP, qty);
+};
+btn.addEventListener("click", profit_loss);
